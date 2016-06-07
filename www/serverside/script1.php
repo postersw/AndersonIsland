5d1c1e3db5b55c44efd18945365141ea1278c914
postersw <postersw@comcast.net>
2016-06-06 19:56:30 -0700

    1.6 Single download for dailycache, comingevents, tides.

---
+++ www/serverside/script1.php
@@ -0,0 +1,29 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////////////
+//  getdailycache.php - retrieves dailycache.txt, comingevents.txt, tidedata.txt
+//  concolidates daily feeds in one big pull, rather than 3 small ones.
+//  rfb. 6/6/16.
+
+copyfile("dailycache.txt", "");
+copyfile("comingevents.txt", "COMINGEVENTS");
+copyfile("tidedata.txt", "TIDES");
+// log it
+$log = 'dailycachelog.txt';
+$tlh = fopen($log, 'a');
+fwrite($tlh, date('c') . 'access from ' . $_SERVER['REMOTE_ADDR'] . "\n");
+return;
+
+// copyfile from file to stdout
+//  diskfile = file name
+//  label = label that surrounds the content
+function copyfile($diskfile, $label) {
+    $theData = file_get_contents($diskfile);
+    if($theData <> "" ) {
+        if($label <> "" ) echo $label . "\n";
+        echo $theData;
+        if($label <> "" ) echo $label . "END\n";
+    }
+}
+
+
+?>
