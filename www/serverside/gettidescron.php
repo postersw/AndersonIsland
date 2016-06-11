4f25d00a7027fe0d2dcd68984296cd14780b6ae6
Robert Bedoll <support@postersw.com>
2016-06-11 10:40:35 -0700

    change gettidescron.php to get 8 days of tides.

--- www/serverside/gettidescron.php
+++ www/serverside/gettidescron.php
@@ -7,7 +7,7 @@
 //  Called by cron every 6 hours (4 times/day).  This gets around the 750 hits/day limit on the free account.
 //  rfb. 6/4/16. 6/6/16.
 //
-    $link = "http://api.aerisapi.com/tides/9446705?client_id=pSIYiKH6lq4YzlsNY54y0&client_secret=vMb1vxvyo7Z96DSn7niwxVymzOxPN6qiEEdBk7vS&from=-15hours&to=+96hours";
+    $link = "http://api.aerisapi.com/tides/9446705?client_id=pSIYiKH6lq4YzlsNY54y0&client_secret=vMb1vxvyo7Z96DSn7niwxVymzOxPN6qiEEdBk7vS&from=-15hours&to=+8days";
     $file = "tidedata.txt";
     chdir("/home/postersw/public_html");  // move to web root
     $str = file_get_contents($link);
