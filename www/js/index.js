21308de3141ec00450bc3533a51a74fca3378846
postersw <postersw@comcast.net>
2017-03-07 12:06:39 -0800

    Version set to 1.8.030717.

--- www/js/index.js
+++ www/js/index.js
@@ -50,7 +50,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var gVer = "1.08.0831030717";  // VERSION MUST be n.nn. ...  e.g. 1.07 for version comparison to work.
+var gVer = "1.08.030717";  // VERSION MUST be n.nn. ...  e.g. 1.07 for version comparison to work.
 var gMyVer; // 1st 4 char of gVer
 
 var app = {
