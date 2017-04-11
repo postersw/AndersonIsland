e8d637c779de983a1c0073025e62e578bbf65c58
postersw <postersw@comcast.net>
2017-04-10 21:47:59 -0400

    0410175 comments.

--- www/js/index.js
+++ www/js/index.js
@@ -57,7 +57,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var gVer = "1.11.0410171";  // VERSION MUST be n.nn. ...  e.g. 1.07 for version comparison to work.
+var gVer = "1.11.0410175";  // VERSION MUST be n.nn. ...  e.g. 1.07 for version comparison to work.
 var gMyVer; // 1st 4 char of gVer
 
 var app = {
