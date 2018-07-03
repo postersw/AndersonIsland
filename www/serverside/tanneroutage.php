8a86ac68360c61ea2e422e456c71bfa8359f7e31
Robert Bedoll <support@postersw.com>
2018-07-03 12:44:52 -0700

    Working ersion of tanneroutage.php

--- www/serverside/tanneroutage.php
+++ www/serverside/tanneroutage.php
@@ -1,11 +1,14 @@
 <?php
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//  tanneroutage - called by Tanner with a post that returns an xml file of the form:
-//  xml version="1.0"
-//  <nbrOut>20</nbrOut>
-//  <nbrServed>1000</nbrServed>
+//  tanneroutage - called by Tanner with a post that returns xml of the form:
+//   [<?xml_version] => "1.0"
+//<outageSummary xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+// coopID="0"><totals><nbrOut>0</nbrOut><nbrServed>4862</nbrServed></totals>
+// <regions type="County"><region><id>North Bend</id><name></name><nbrOut>0</nbrOut><nbrServed>2175</nbrServed></region>
+// <region><id>Ames Lake</id><name></name><nbrOut>0</nbrOut><nbrServed>1481</nbrServed></region
+//<region><id>Anderson Island</id><name></name><nbrOut>0</nbrOut><nbrServed>1206</nbrServed></region></regions></outageSummary>
 
-//if ($request->getMethod() !== 'POST') {
+//if ($_REQUEST[REQUEST_METHOD] !== 'POST') {
 //    http_response_code(405);
 //    exit('Use POST method.');
 //}
@@ -18,27 +21,26 @@ $uploadOk = 1;
 date_default_timezone_set("America/Los_Angeles"); // set PDT
 
 // move the file without having to know its original name. Get the name from the array.
-echo "<pre>";
-print_r($_FILES);
-foreach($_FILES as $file) {
-    echo "key=value: " . $file . " tmp_name: ". $file['tmp_name'] . "\n";
-    $r = move_uploaded_file($file['tmp_name'], $target_file);
+$logit = print_r($_FILES, TRUE) . "," . print_r($_POST, TRUE) . "," . print_r($_REQUEST, TRUE) . "," . print_r($_SERVER, TRUE);
+// get the $POST reqeust as $xml
+foreach($_POST as $p) {
+    $xml = $p;
     break;
 }
+$logit = $logit . "\n XML: " . $xml;
 
-$msg = ProcessXMLFile($target_file);
-file_put_contents($logfile, date("Y/m/d H:i:s") .  $msg);  // log it
+$msg = ProcessXMLFile($xml);
+file_put_contents($logfile, date("Y/m/d H:i:s") .  $msg . $logit );  // log it
 if(!strpos($msg, "ERR:") ) file_put_contents($outagefile, $msg);  // write to outage file
-echo "<br/>" . $msg . "<br/>";
 exit;
 
 ///////////////////////////////////////////////////////////////////////////////////////
-//  ProcessXMLFile (filename)  read the tanner xml file and decode it
+//  ProcessXMLFile (string)  read the tanner xml post and decode it
 //  returns string with the outage message, OR "ERR <error message>"
 //  looks for tags <nbrOut>  and <nbrServed>
-//  Entry $f = file name to read
+//  Entry $s = xml string
 //  Exit: returns string with the outage message, OR "ERR <error message>"
-//function ProcessXMLFile($f) {
+//function ProcessXMLFile($s) {
 //    $s = file_get_contents($f);
 //    echo "<pre>" . $s . "</pre>";
 //    $xml=simplexml_load_string($f);
@@ -57,18 +59,22 @@ exit;
 //}
 
 ///////////////////////////////////////////////////////////////////////////////////////
-//  ProcessXMLFile (filename)  read the tanner xml file and decode it
+//  ProcessXMLFile (xml string)  read the tanner xml string and decode it
 //  returns string with the outage message, OR "ERR <error message>"
 //  looks for tags <nbrOut>  and <nbrServed>
 //  Entry $f = file name to read
 //  Exit: returns string with the outage message, OR "ERR <error message>"
-function ProcessXMLFile($f) {
-    $s = file_get_contents($f);
+function ProcessXMLFile($s) {
+    global $logit;
+    //$s = file_get_contents($f);
     //echo "<pre>" . $s . "</pre>";
-    if(!strpos($s, "<outageSummary")) return "ERR: not outageSummary";
-    $nbrOut = TagValue($s, "nbrOut");  // get the tag value
-    $nbrServed = TagValue($s, "nbrServed");  // get the tag value
-    echo "nbrout: " . $nbrOut . ", nbrserved: " . $nbrServed . "<br/>";
+    $i=strpos($s, "<outageSummary");
+    if($i===FALSE) return "ERR: not outageSummary";
+    $i = strpos($s, "Anderson Island");
+    if($i===FALSE) return "ERR: Anderson Island not found";
+    $nbrOut = TagValue($s, "nbrOut", $i);  // get the tag value
+    $nbrServed = TagValue($s, "nbrServed", $i);  // get the tag value
+    $logit = $logit . "\n nbrout: " . $nbrOut . ", nbrserved: " . $nbrServed . "\n";
     if((int)$nbrOut == 0) {  // if no outages
         $msg =  date("H:i") . ": No Outages.";
     } else {
@@ -82,12 +88,12 @@ function ProcessXMLFile($f) {
 //  entry $s = string
 //          $tag = xml tag without <>
 // Returns  string between tags, or error message
-function TagValue($s, $tag) {
-    $i = strpos($s, "<" . $tag . ">");
-    if($i==0) return "ERR: <" . $tag . "> not found";
+function TagValue($s, $tag, $is) {
+    $i = strpos($s, "<" . $tag . ">", $is);
+    if($i==FALSE) return "ERR: <" . $tag . "> not found";
     $taglen = strlen($tag) + 2;
     $j = strpos($s,  "</" . $tag . ">", $i);
-    if($j==0) return "ERR: </".  $tag . "> not found";
+    if($j==FALSE) return "ERR: </".  $tag . "> not found";
     return substr($s, $i+$taglen, $j-$i-$taglen);
 }
 
