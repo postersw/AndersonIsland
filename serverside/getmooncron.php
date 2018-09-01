03827c220cb445e762e64d59487068b5f85230b0
Robert Bedoll <support@postersw.com>
2018-09-01 12:17:27 -0700

    Make mooncron get tomorrow's data to pick up set time.

--- serverside/getmooncron.php
+++ serverside/getmooncron.php
@@ -5,8 +5,9 @@
 //  this file is picked up by the the app directly.
 //  Called by cron every morning at 1am.
 //  Bob Bedoll. 8/18/18.
+//  9/1/18. reissue request for tomorrow if necessary to get set time.
 //
-//  API REST CALL: http://api.usno.navy.mil/rstt/oneday?date=8/18/2018&loc=Seattle,%20WA
+//  API REST CALL: http://api.usno.navy.mil/rstt/oneday?date=today&loc=Seattle,%20WA
 //  API RETURN:
 //  {
 //"error":false,
@@ -34,11 +35,15 @@
 //"moondata":[
 //            {"phen":"R", "time":"2:41 p.m. DT"},
 //            {"phen":"U", "time":"7:36 p.m. DT"}],
-
+// SOMETIMES I GET:
 //"nextmoondata":[
 //            {"phen":"S","time":"12:25 a.m. DT"}],
-
-//"closestphase":{"phase":"First Quarter","date":"August 18, 2018","time":"12:48 a.m. DT"}
+// BUT SOMETIMES I HAVE TO REISSUE THE REQUEST FOR TOMORROW AND USE THE SET TIME IN MOONDATA.
+//"closestphase":{"phase":"First Quarter","date":"August 18, 2018","time":"12:48 a.m. DT"},
+// SOMETIMES I GET (if it is not a full moon):
+//  "fracillum":"64%", 
+// SOMETIMES I GET:
+//  "curphase":"Waning Gibbous"
 //}
 //
 $file = "moondata.txt";
@@ -47,26 +52,32 @@ chdir("/home/postersw/public_html");  // move to web root
 // code to get moon rise/set & phase
 
 date_default_timezone_set("America/Los_Angeles"); // set PDT
-$link = "http://api.usno.navy.mil/rstt/oneday?loc=Seattle,%20WA&date=";
-$ts = time();
-$y = date("Y", $ts); // year, e.g. 2017
-$m = date ("m", $ts); // month with leading zero
-$d = date("d", $ts); // day with leading zero
-$link = $link . $m . "/" . $d . "/" . $y;
+$link = "http://api.usno.navy.mil/rstt/oneday?loc=Seattle,%20WA&date=today";
+$linktomorrow = "http://api.usno.navy.mil/rstt/oneday?loc=Seattle,%20WA&date=tomorrow";
+
+//$ts = time();
+//$y = date("Y", $ts); // year, e.g. 2017
+//$m = date ("m", $ts); // month with leading zero
+//$d = date("d", $ts); // day with leading zero
+//$link = $link . $m . "/" . $d . "/" . $y;
 echo $link; //debug
 
 
-// retry 10 times
-for ($x = 0; $x <= 10; $x++) {
-    $str = "";
-    $str = file_get_contents($link);
-    if($str != "") break;
-}
-if($str == "") {  // if no data
-    echo("moon cron run: NO usno.navy.mil moon DATA after 10 tries!!!");
-    return 0;
-}
+// Get the moon data - retry 10 times
+$str = GetData($link);
+//$strtomorrow = GetData($linktomorrow);
+//for ($x = 0; $x <= 10; $x++) {
+//    $str = "";
+//    $str = file_get_contents($link);
+//    if($str != "") break;
+//}
+//if($str == "") {  // if no data
+//    echo("moon cron run: NO usno.navy.mil moon DATA after 10 tries!!!");
+//    return 0;
+//}
 
+
+// reformat the reply
 $strout = FormatHtml($str);  // reformat
 echo "<br/>$strout<br/>";
 $j = file_put_contents($file, $strout);  // save the data
@@ -81,46 +92,71 @@ return 0;
 // formathtml - reformat data from usno.navy.mil format to HTML line to be output directly by app.
 //  input: see format above
 //  output: Moon: Rise hh:mm, Set hh:mm, ICON for PHASE
+//  NOTE: will issue a request to navy.mil for tomorrow data if necessary to get set time
+//  NOTE: Rise time is always in the 'today' data (moondata "R").
+//      Set time could be in today data (moondata "S") after rise, OR
+//          today data (nextmoondata "S") OR
+//          tomorrow data HTML request  (moondata "S")
 
 function FormatHtml($reply) {
+    global $linktomorrow;
     $jreply = json_decode($reply);  // decode the json reply
-    // rise time today:
+    if(is_null($jreply)) echo "<br/>Error decoding Today json data<br/>";
 
-    $st = "";
+    // find rise time today:
+    $risetime = "";
     for($i=0; $i<count($jreply->moondata); $i++) {
         if($jreply->moondata[$i]->phen == "R") {
-            $st = $jreply->moondata[$i]->time;
+            $risetime = $jreply->moondata[$i]->time;
             break;
         }
     }
     // if an error
-    if ($st == "") {
+    if ($risetime == "") {
         echo "ERROR - no moon rise returned. \n";
         die( "ERROR - no moon rise returned.");
-    }
-    // possible set time today
-    $et = "";
+    } else echo "<br/>Moon Rise: $risetime <br/>";
+
+    // find possible set time today
+    $settime = "";
     for($i++; $i<count($jreply->moondata); $i++) {
         if($jreply->moondata[$i]->phen == "S") {
-            $et = $jreply->moondata[$i]->time;
-            echo "<br/>Moon sets today $et<br/>";
+            $settime = $jreply->moondata[$i]->time;
+            echo "<br/>Moon sets today $settime<br/>";
             break;
         }
     }
-    // not setting today. get set time tomorrow.
-    if($et == "") {
+    // not setting today. find set time tomorrow.
+    if($settime == "") {
         for($i=0; $i<count($jreply->nextmoondata); $i++) {
             if($jreply->nextmoondata[$i]->phen == "S") {
-                $et = $jreply->nextmoondata[$i]->time;
-                echo "<br/>Moon sets tomorrow $et<br/>";
+                $settime = $jreply->nextmoondata[$i]->time;
+                echo "<br/>Moon sets tomorrow $settime<br/>";
                 break;
             }
         }
     }
-    echo $st, $et;
+
+    // no set time for tomorrow present in the reply, so get and use tomorrow data
+    if($settime == "") {
+        echo "<br/>Getting moon data for tomorrow 1<br/>";
+        $str = GetData($linktomorrow);  // ask the navy for tomorrows data which will have the set time for tonight
+        if($str == "" || $str==false) echo "<br/>No tomorrow data returned for $link <br/>";
+        $jreplytomorrow = json_decode($str);  // decode the json reply
+        if(is_null($jreplytomorrow)) echo "<br/>Error decoding Tomorrow json data<br/>";
+        for($i=0; $i<count($jreplytomorrow->moondata); $i++) {
+            if($jreplytomorrow->moondata[$i]->phen == "S") {
+                $settime = $jreplytomorrow->moondata[$i]->time . " tomorrow";
+                echo "<br/>Moon sets tomorrow $settime<br/>";
+                break;
+            }
+        }
+    }
+
+    echo $risetime, $settime;
     // if an error
-    if ($st == "") {
-        echo "ERROR - no moon rise returned. \n";
+    if ($risetime == "") {
+        echo "<br/>ERROR - no moon rise returned. <br/>";
         die( "ERROR - no moon rise returned.");
     }
     // get phase and icon
@@ -143,13 +179,30 @@ function FormatHtml($reply) {
     }
     //  final return;
     if($pct == "") $pct = $mp; // if no pct, use the phase
-    $s = "<br/>Moon:<span style='font-weight:normal'> <img style='vertical-align:middle' src='img/$icon.png' width=30 height=30> $pct, Rise: $st | Set: $et</span> ";
+    $s = "<br/>Moon:<span style='font-weight:normal'> <img style='vertical-align:middle' src='img/$icon.png' width=30 height=30> $pct, Rise: $risetime | Set: $settime</span> ";
     // fix times
     $s = str_replace("a.m.", "a", $s);
     $s = str_replace("p.m.", "p", $s);
     $s = str_replace("DT", "", $s);
     $s = str_replace("ST", "", $s);
     return $s . "";
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Get data after 10 tries
+//  Entry   link = address
+//  returns data
+function GetData($link) {
+    echo $link;
+    for ($x = 0; $x <= 10; $x++) {
+        $str = "";
+        echo " GetData $x. ";
+        $str = file_get_contents($link);
+        echo "str=$str";
+        if($str != false && $str != "") {echo "returned ok<br/>"; return $str;}
+    }
+    echo("<br/>moon cron run: NO usno.navy.mil moon DATA after 10 tries for $link<br/>");
+    return "";
 }
 
 ?>
