d6ea5befd121cc805139b422c422f30ff1b4bc1d
Robert Bedoll <support@postersw.com>
2018-11-20 12:37:14 -0800

    IOS DEBUG

--- www/js/index.js
+++ www/js/index.js
@@ -4571,8 +4571,11 @@ function SetTideTitle() {
 //  MAIN APP CODE
 //
 function StartApp() {
+    Show("vermsg"); // display the version  DEBUG
     app.initialize();
-    alert("at 0");//////////////////////////////
+    Show("mainpage");  // now display the main page DEBUG
+
+    alert("at 0");//////////////////////////////DEBUG
     initializeMobile(); // set flags
     FixiPhoneHeader();
     document.getElementById("versionnumber").innerHTML = "&nbsp&nbsp AIA Ver: " + gVer; // version stamp on footer
@@ -4589,7 +4592,7 @@ function StartApp() {
     document.getElementById("updatetime").innerHTML = "Updated " + FormatTime(gTimehhmm);
     gForceCacheReload = false; // cache reload not needed
     gForceTideReload = false;
-    alert("at 1");//////////////////////////////
+    alert("at 1");//////////////////////////////DEBUG
     InstallAvailable();  // point user to google play only if a mobile browser that is NOT PhoneGap
     UpdateAvailable(); // point user to google play only if a new version is available
 
@@ -4598,12 +4601,12 @@ function StartApp() {
 
     // ios - hide the update app at the request of the Apple App Review team 3/19/17.
     if (isPhoneGap() && !isAndroid()) document.getElementById("updateappswitch").setAttribute('style', 'display:none;');
-    alert("at 2");//////////////////////////////
+    alert("at 2");//////////////////////////////DEBUG
     // Replace icons with Labels if user selected them
     InitializeIcons();
     TXTS.InitializeSpeechMessage(); // initial speech message
     BIGTX.InitializeBigText(); // initial big text
-    alert("at 3");//////////////////////////////
+    alert("at 3");//////////////////////////////DEBUG
     //  Show the cached data immediately if there is no version change. Otherwise wait for a cache reload.
     if(LSget("myver") == gMyVer) {
         ParseFerryTimes();  // moved saved data into ferry time arrays
@@ -4616,7 +4619,7 @@ function StartApp() {
     //Show("vermsg"); // display the version
 
     // -------------  after main page has been displayed ---------------------------
-    alert("at 4");//////////////////////////////
+    alert("at 4");//////////////////////////////DEBUG
     //reload the 'dailycache' cache + coming events + tides + forecast if the day or MyVer has changed .
     var reloadreason = "";
     var dailycacheloaded = localStorage.getItem("dailycacheloaded");
@@ -4642,7 +4645,7 @@ function StartApp() {
         getForecast(); // updates forecast every 2 hrs
 
     }
-    alert("at 5");//////////////////////////////
+    alert("at 5");//////////////////////////////DEBUG
     if (isPhoneGap()) {
         s = localStorage.getItem("ferryhighlight");
         if (s == null) document.getElementById("locationdialog").style.width = "100%";// the 1st time, ask user for permission
@@ -4657,14 +4660,14 @@ function StartApp() {
     document.addEventListener("pause", onPause, false);
     document.addEventListener("resume", onResume, false);
     //DisplayLoadTimes();
-    alert("at 6");//////////////////////////////
+    alert("at 6");//////////////////////////////DEBUG
     MenuSetup(); // setup the menu switches
 
     // show the page
     Show("mainpage");  // now display the main page
     Show("vermsg"); // display the version
-    alert("at 7");//////////////////////////////
+    alert("at 7");//////////////////////////////DEBUG
     // initial alerts are shown now (otherwise they cause a timeout error)
     if (TXTS.FirstTime) TXTS.FirstTimeMsg();
-    alert("at 8");//////////////////////////////
+    alert("at 8");//////////////////////////////DEBUG
 }
