249bd83fa2f18e9f2ef49c8913e1f4cf648de6c5
Robert Bedoll <support@postersw.com>
2019-03-26 10:08:22 -0700

    update burn ban status

--- serverside/getburnbanalerts.php
+++ serverside/getburnbanalerts.php
@@ -11,6 +11,7 @@
 //  Fire department: look for "Current Fire Safety Burn Ban Status". Then for "NO BURN BAN".
 //      Rev 2/16/18.
 //      Rev 2/5/18. Look for NO FIRE SAFETY BURN BAD
+//      Rev 3/26/19: Look for: Current Fire Safety Burn Ban Status:</p><p>No Burn Ban
 //
     $burnbanlink = "http://wc.pscleanair.org/burnban411/";
     $firebblink = "http://www.co.pierce.wa.us/982/Burn-Bans";
@@ -59,8 +60,14 @@
     }
     if($i == 0) Bailout("Could not find \"$cwbb\"");
 
+    $lifted = 0; // >0 if no burn ban
+    $effective = 0; // >0 if there is a burn ban
+
+    // 3/26/19: Find the line after Current Fire Safety Burn Ban:
+    $lifted = stripos($fire, "Fire Safety Burn Ban Status:</p><p>No Burn Ban", $i);
+
     // now find alt image tags (bad solution) to get the actual status
-    $lifted = stripos($fire, "FIRE SAFETY - NO BURN BAN", $i); // these are alt image tags, which will change.
+    if($lifted===false) $lifted = stripos($fire, "FIRE SAFETY - NO BURN BAN", $i); // these are alt image tags, which will change.
     if($lifted===false) $lifted = stripos($fire, "NO FIRE SAFETY BURN BAN", $i);
     $effective = stripos($fire, '"FIRE SAFETY - BURN BAN"', $i);
     if($effective===false) $effective = stripos($fire, '"BURN BAN IN EFFECT"', $i);
