6eda2bed66fca5cf57dfed879ef2afd923054c7d
Robert Bedoll <support@postersw.com>
2019-04-08 06:28:28 -0700

    Revise getferryalerts to store api key in private directory.

--- serverside/pushtest.php
+++ serverside/pushtest.php
@@ -5,40 +5,48 @@ $msg = "This is a test of OneSignal messaging. Please ignore.";
 
 PushOSNotification($title, $msg);
 return;
-/////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
 //  PushOneSignalNotification. 5/25/18
 //  Entry   title = message title
 //          msg = the message
 //  Users curl library.
 //  https://documentation.onesignal.com/reference#create-notification
+//  7/9/18: ios_badgeCount set to 0
+//  4/7/19: Rest API Key moved from code to file in root after key was hacked.
 //
 function PushOSNotification($title, $msg) {
-		$fields = array(
-			'app_id' => "a0619723-d045-48d3-880c-6028f8cc6006",
-			'included_segments' => array('Active Users'),
-            'headings' => array("en" => $title),
-			'contents' => array("en" => $msg),
-            'ttl' => 4*3600
-		);
-		$fields = json_encode($fields);
-    	print("\nJSON sent:\n");
-    	print($fields);
+    require ('../private/OneSignal.php');
+    $fields = array(
+        'app_id' => "a0619723-d045-48d3-880c-6028f8cc6006",
+        'included_segments' => array('Test Users'),
+        'headings' => array("en" => $title),
+        'contents' => array("en" => $msg),
+        'ttl' => 4*3600,
+        'ios_badgeType' => 'SetTo',
+        'ios_badgeCount' => 0
+    );
+    $fields = json_encode($fields);
+    print("\nJSON sent:\n");
+    print($fields);
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-												   'Authorization: Basic YWQyZmE5OGUtNGY0MC00OTAyLWEyOTYtMTUyZjVjZjEyNzA0'));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ch, CURLOPT_HEADER, FALSE);
-		curl_setopt($ch, CURLOPT_POST, TRUE);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+    // "Authorization: Basic YOUR_REST_API_KEY (from the OneSignal web site for my app). After 4/7/19 this key must be set
+    //  before the code is uploaded to the web site.
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8', $RestAPIKey));
+    //                                           'Authorization: Basic YWQyZmE5OGUtNGY0MC00OTAyLWEyOTYtMTUyZjVjZjEyNzA0'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
-		$response = curl_exec($ch);
-		curl_close($ch);
-        print($response);
-		return $response;
-	}
+    $response = curl_exec($ch);
+    curl_close($ch);
+    print($response);
+    return $response;
+}
 
 /////////////////////////////////////////////////////////////////////////
 // PUSHBOTS
