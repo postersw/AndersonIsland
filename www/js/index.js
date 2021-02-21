502c106ed5b1d84668fc551e04dcba1a6ff2474f
Robert Bedoll <support@postersw.com>
2021-02-20 19:51:22 -0500

    Change ketron color back to gray for ferry schedule main page.

--- www/js/index.js
+++ www/js/index.js
@@ -97,7 +97,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-const gVer = "1.30.021621";  // VERSION MUST be n.nn. ...  e.g. 1.07 for version comparison to work.
+const gVer = "1.30.021921";  // VERSION MUST be n.nn. ...  e.g. 1.07 for version comparison to work.
 var gMyVer; // 1st 4 char of gVer
 const cr = "copyright 2016-2021 Robert Bedoll, Poster Software LLC";
 
@@ -1923,7 +1923,7 @@ function FindNextFerryTime(ferrytimes, ferrytimeK, SA) {
     if (i >= ferrytimes.length) ft = ft + FindNextFerryTimeTomorrow(SA, nruns);
 
     // ketron only if there is a ketron run, and it is valid. note iketron ponts to 1st run
-    if ((ferrytimeK != null) && ketron) ft = ft + "</tr><tr style='font-weight:bold;color:brown'><td style='padding:0px;margin:0;'>Ketron:</td>" + ketront;
+    if ((ferrytimeK != null) && ketron) ft = ft + "</tr><tr style='font-weight:bold;color:darkgray'><td style='padding:0px;margin:0;'>Ketron:</td>" + ketront;
     return ft;
 }
 
