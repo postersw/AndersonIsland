d683f1b90f3d59f9b31a0855c4466d7347308a6c
postersw <support@postersw.com>
2021-04-27 23:55:44 -0700

    Display all rows for a day.

--- reference/overflowcameras.php
+++ reference/overflowcameras.php
@@ -2,6 +2,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  overflowcameras.php  display the overflow cameras for lanes and dock
 //  overflowcameras.php?f=Adhhmm         where A = A or S, d = 1-7, hhmm = 24 hr time
+//                      f=d where d=1-7 for all cameras for a day
 //  Called by displayferryoverflow.php
 //  Entry: files stored in /Overflow by getferryovereflow.php
 //  File name is Adhhmm.jpg or Sdhhmm.jpg for lanes camers
@@ -18,7 +19,20 @@ $Day = array("", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturd
     echo '</head><body><div>';
     
     chdir("/home/postersw/public_html/Overflow");
+
+    // if f=d, display pictures for one day
+
     $f = $_GET["f"];
+    if(!ctype_alnum($f)) exit("invalid"); // prevent invalid characters
+    if($f == "") exit(0);
+    $s = strlen($f);
+    if($s==1) {  // 1 character is the day only
+        OneDay($f);
+        exit(0);
+    }
+    if($s != 6) exit(); // must be 6
+
+    // display cameras for the explicit time
     if(substr($f,0,1) == "S") $dock = "Steilacoom";
     else $dock = "Anderson Island";
     $dt = file_get_contents("L$f.txt");
@@ -30,6 +44,37 @@ $Day = array("", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturd
     echo "($dt)";
     echo "</div></body></html>";
     exit(0);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//  All cameras for a day
+//  entry   $d = day index, 1=7
+function OneDay($d) {
+    global $Day;
+    $ST = array(445,545,705,820,930,1035,1210,1445,1550,1700,1810,1920,2035,2220); // ST departures
+    $AI = array(515,620,735,855,1005,1110,1245,1515,1625,1735,1845,1955,2110,2250); // AI departures
+
+    $s = 0;
+    if($d >5 ) $s = 1; // skip early runs on sat, sun
+    echo "Overflow on $Day[$d] for Steilacoom: <br/> ";
+    for($i=$s; $i<count($ST); $i++){
+        echo formattime($ST[$i]) . ":<br/>";
+        $f = "S" . $d . sprintf('%04d', $ST[$i]);
+        echo "<img src='Overflow/$f.jpg'></img> ";
+        echo "<img src='Overflow/D$f.jpg'></img><br/>";
+        $dt = file_get_contents("L$f.txt");
+        echo "($dt)<hr/>";
+    }
+
+    echo "<hr/>Overflow on $Day[$d] for Anderson Island: <br/> ";
+    for($i=$s; $i<count($AI); $i++){
+        echo formattime($AI[$i]) . ":<br/>";
+        $f = "A" . $d . sprintf('%04d', $AI[$i]);
+        echo "<img src='Overflow/$f.jpg'></img> ";
+        echo "<img src='Overflow/D$f.jpg'></img><br/>";
+        $dt = file_get_contents("L$f.txt");
+        echo "($dt)<hr/>";
+    }
+}
 
 //////////////////////////////////////////////////////////////////
 //  formattime convert interger time to display time with am/pm
