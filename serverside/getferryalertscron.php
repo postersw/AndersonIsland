0030554993b156000e546be31a3f4f6fcad4b96b
Robert Bedoll <support@postersw.com>
2022-11-01 10:25:06 -0700

    No alerts midnight-4am

--- serverside/getferryalertscron.php
+++ serverside/getferryalertscron.php
@@ -66,6 +66,10 @@ $alertlog = "alertlog.txt";
 $alerthistory = "alerthistory.txt";
 $alerttimestampfile = "alerttimestamp.txt";
 
+// skip alerts midnight - 4am
+$h =intval(date("H"));   // hour
+if($h < 4) exit(0);
+
 //  Get a new alert. returned in $AlertObj
 //  Always returns new alert only once!
 $AlertObj = getNewestAlertfromHornblower();  //
